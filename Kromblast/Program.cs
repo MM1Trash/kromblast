29b269dbeab8c7aa99b7f249d45f17eccb956c12
MisterMine01 <pokemovie06@gmail.com>
2022-10-05 20:31:00 +0200

    well-no

---
+++ Kromblast/Program.cs
@@ -0,0 +1,15 @@
+﻿using WebWindows;
+// See https://aka.ms/new-console-template for more information
+
+namespace Kromblast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var window = new WebWindow("My super app");
+            window.NavigateToString("<h1>Hello, world!</h1> This window is from a .NET Core app.");
+            window.WaitForExit();
+        }
+    }
+}
